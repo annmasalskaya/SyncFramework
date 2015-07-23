9a7441fc0527cc29d49bd84df9b9a54c3b6179c2
Artem Jackson <artemjackson95@gmail.com>
2015-07-23 13:31:19 +0300

    Added IVersionEntity

---
+++ SF/Entites.Interfaces/IVersionEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF.Entites
+{
+    interface IVersionEntity
+    {
+        int Version { get; set; }
+    }
+}
