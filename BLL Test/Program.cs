db99e41be3e7b88d0c04cd96a1c9124f2d12d371
annmasalskaya <mas.anna25@gmail.com>
2015-07-27 22:22:24 +0300

    Added project for testing BLL with VersionControlService

---
+++ BLL Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+using DependencyResolver;
+using BLL.Interfaces.Services;
+using DAL.Entites;
+
+namespace BLL_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kernel = new StandardKernel(new DependencyResolverModule());
+            
+            var service = kernel.Get<IVersionControlService<User>>();
+            var result = service.GetDeletedAfter(new DateTime(2012, 4, 12));
+            Console.WriteLine(result.Count());
+            Console.ReadLine();
+        }
+
+    }
+}
